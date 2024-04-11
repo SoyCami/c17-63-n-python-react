a2fb0a9dc479c05a9ed68140ab08e432a16e8548
Maria Camila Bohórquez Chica <maria.bohorquez@avenfor.com>
2024-04-10 21:26:53 -0500

    Chore(Add components): Realizar componentes

---
+++ app-front/src/components/atoms/Btn/Btn.tsx
@@ -0,0 +1,29 @@
+import {  ReactElement } from 'react';
+import { type BtnM } from './Btn.model';
+import classNames from './Btn.module.css'
+
+export const Btn = ({
+    onClick,
+    className,
+    children,
+    type,
+    text,
+
+}: BtnM): ReactElement => {
+    const BtnStyles: React.CSSProperties = {
+        
+    }
+        return (
+        <button
+            className={`${classNames.btn} ${className ?? ''}`}
+            style={BtnStyles}
+            type={type}
+            onClick={onClick}
+            >
+            {text ?? children}
+        </button>
+    );
+
+};
+
+
