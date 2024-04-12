9af2ff05b3f5238cbad3173017cc591f214d087b
Miguel Angel Rizzi <123975192+MiguelRizzi@users.noreply.github.com>
2024-04-12 18:51:52 -0300

    Integrate OpenAPI (#10)

--- core/urls.py
+++ core/urls.py
@@ -19,10 +19,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("events/", include(("events.urls", "events"))),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/v1/", include("apps.users.urls", "users")),
 ]
 
