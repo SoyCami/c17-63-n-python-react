facc3f6364850d449f2dd71c92645987d9b43f3c
MelanyPalacio <melanypalacio.mp@gmail.com>
2024-04-25 00:43:57 -0500

    Completed Main

--- app-front/next.config.js
+++ app-front/next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 module.exports = {
     images: {
-      domains: ['via.placeholder.com', '63pythonreact.pythonanywhere.com'],
+      domains: ['via.placeholder.com', '63pythonreact.pythonanywhere.com', 'img.freepik.com', 'i.pinimg.com'],
     },
   }
   
