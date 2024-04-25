a0a0ec004f348ab7b6c782d1190f93c6f3aab46a
MelanyPalacio <melanypalacio.mp@gmail.com>
2024-04-25 00:51:40 -0500

    Main Completed with function

--- app-front/src/app/page.tsx
+++ app-front/src/app/page.tsx
@@ -1,10 +1,6 @@
 import Footer from "@/components/layouts/Footer/Footer";
 import Main from '@/app/Main/Page/Main'
 import HeaderScreen from '@/components/layouts/HeaderScreen/HeaderScreen';
-import TopEvents from "@/app/TopEvents/page";
-import Header from "@/components/layouts/Header/Header";
-import MainScreen from "@/app/MainScreen/Page";
-import FooterScreen from "@/components/layouts/FooterScreen/FooterScreen";
 
 
 export default function Home() {
