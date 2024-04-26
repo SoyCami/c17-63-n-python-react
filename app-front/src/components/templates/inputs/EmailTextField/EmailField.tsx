d10598df98e84aad47745c716825f78c22797d51
Maria Camila Bohórquez Chica <maria.bohorquez@avenfor.com>
2024-04-26 12:52:05 -0500

    Add(Conexiones)

--- app-front/src/components/templates/inputs/EmailTextField/EmailField.tsx
+++ app-front/src/components/templates/inputs/EmailTextField/EmailField.tsx
@@ -1,17 +1,22 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useState, ChangeEvent } from 'react';
 
-export default function EmailField (): ReactElement {
-  const [email, setEmail] = useState('');
+interface EmailFieldProps {
+  value?: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function EmailField(props: EmailFieldProps): ReactElement {
+  const { value, onChange } = props;
   const [error, setError] = useState('');
 
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange(event);
     const isValid = validateEmail(event.target.value);
     setError(isValid ? '' : 'Ingresa un correo electrónico válido');
   };
 
   const validateEmail = (emailValue: string) => {
-    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z\-0-9]{2,}))$/;
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(emailValue);
   };
 
@@ -21,7 +26,7 @@ export default function EmailField (): ReactElement {
       <input
         type="email"
         id="email"
-        value={email}
+        value={value}
         onChange={handleEmailChange}
         className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal text-black"
         placeholder="Ingresa tu correo electrónico"
@@ -30,5 +35,4 @@ export default function EmailField (): ReactElement {
       <div className="text-red-500 mt-2">{error}</div>
     </div>
   );
-};
-
+}
